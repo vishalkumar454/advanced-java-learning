fe70f28e8f55a1dfe093f027f75994f5a05cb379
vishalkumar <rajvishal71760@gmail.com>
2026-07-24 14:05:16 +0530

    implemented packages for performing DDl statement

---
+++ 01-JDBC-CRUD/src/main/java/com/postgreSql/DDL/CopyTable.java
@@ -0,0 +1,29 @@
+package com.postgreSql.DDL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CopyTable {
+	public static void main(String[] args) {
+		
+		String url = "jdbc:postgresql://localhost:5432/mydb?user=postgres&password=root";
+		String query = "CREATE TABLE emp AS SELECT * FROM employee";
+				
+		try(Connection con = DriverManager.getConnection(url)) {
+			Class.forName("org.postgresql.Driver");
+			
+			Statement stm = con.createStatement();
+			
+			stm.execute(query);
+			
+			con.close();
+			System.err.println("connection closed");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
