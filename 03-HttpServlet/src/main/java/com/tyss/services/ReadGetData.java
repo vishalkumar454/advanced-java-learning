876713a52499627944d81dab713010dea8c5aaa2
vishalkumar <rajvishal71760@gmail.com>
2026-07-27 20:25:32 +0530

    Get and post methods are implemented

---
+++ 03-HttpServlet/src/main/java/com/tyss/services/ReadGetData.java
@@ -0,0 +1,35 @@
+package com.tyss.services;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dataf")
+public class ReadGetData extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Reading data from Form using Get method");
+		
+		String name = req.getParameter("username");
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(password);
+		
+		PrintWriter pw = resp.getWriter();
+		pw.print("Welcome to our page");
+		
+	}
+}
---
+++ 03-HttpServlet/src/main/java/com/tyss/services/ReadPostData.java
@@ -0,0 +1,37 @@
+package com.tyss.services;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/data")
+public class ReadPostData extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println("Reading data from Form using Post method");
+
+		String name = req.getParameter("username");
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+
+		System.out.println(name);
+		System.out.println(email);
+		System.out.println(password);
+
+		PrintWriter pw = resp.getWriter();
+		pw.print("Welcome to our page");
+
+	}
+}
