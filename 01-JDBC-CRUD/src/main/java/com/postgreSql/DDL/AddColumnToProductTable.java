43b26082ff7bb20d61b362d6d09ab4d0b7b19ddf
vishalkumar <rajvishal71760@gmail.com>
2026-08-16 12:39:11 +0530

    classes modified

--- 01-JDBC-CRUD/src/main/java/com/postgreSql/DDL/AddColumnToProductTable.java
+++ 01-JDBC-CRUD/src/main/java/com/postgreSql/DDL/AddColumnToProductTable.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class AddColumnToProductTable {
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 	
 		String url = "jdbc:postgresql://localhost:5432/mydb";
 		String username = "postgres";
