4d82206154020012261cd8d8110e1809eab7ba01
vishalkumar <rajvishal71760@gmail.com>
2026-08-16 12:29:20 +0530

    implemented rename column query to product's column

---
+++ 01-JDBC-CRUD/src/main/java/com/postgreSql/DDL/RenameColumnFromProductTable.java
@@ -0,0 +1,27 @@
+package com.postgreSql.DDL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RenameColumnFromProductTable {
+	
+	public static void main(String[] args) {
+		
+		String url = "jdbc:postgresql://localhost:5432/mydb?user=postgres&password=root";
+		String query = "ALTER TABLE PRO "
+				+ "RENAME COLUMN QTY TO QUANTITY";
+				
+		try(Connection con = DriverManager.getConnection(url)) {
+			Class.forName("org.postgresql.Driver");
+			Statement stm = con.createStatement();
+			stm.execute(query);
+			System.out.println("connection closed");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
