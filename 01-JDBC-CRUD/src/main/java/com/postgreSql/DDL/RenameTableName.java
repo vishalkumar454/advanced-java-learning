d61b39c7b655039d25540d44579772bae0ca7fc0
vishalkumar <rajvishal71760@gmail.com>
2026-08-16 12:03:33 +0530

    implemented rename query to rename product table name

---
+++ 01-JDBC-CRUD/src/main/java/com/postgreSql/DDL/RenameTableName.java
@@ -0,0 +1,37 @@
+package com.postgreSql.DDL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RenameTableName {
+	
+	public static void main(String[] args) {
+		
+		String url = "jdbc:postgresql://localhost:5432/mydb";
+		String username = "postgres";
+		String password = "root";
+		String query = "ALTER TABLE IF EXISTS PRODUCT RENAME TO PRO";
+		
+		try(Connection con = DriverManager.getConnection(url,username,password)) {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("driver loaded");
+			
+			System.out.println("connection created");
+			
+			Statement stm = con.createStatement();
+			System.out.println("statement created");
+			
+			boolean execute = stm.execute(query);
+			System.out.println("query executed");
+			
+			System.out.println("connection closed");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
