fcc3f4cbd1acd835b2e4676a12a61c84c0fe7ecf
vishalkumar <rajvishal71760@gmail.com>
2026-08-23 00:14:40 +0530

    testing the method

--- 03-HttpServlet/src/main/java/com/tyss/services/ReadGetData.java
+++ 03-HttpServlet/src/main/java/com/tyss/services/ReadGetData.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/dataf")
+@WebServlet("/data")
 public class ReadGetData extends HttpServlet {
 	/**
 	 * 
