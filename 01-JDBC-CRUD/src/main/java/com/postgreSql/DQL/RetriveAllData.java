f4956741cb360fdd33902b147fa38dd72f413a73
vishalkumar <rajvishal71760@gmail.com>
2026-08-23 22:39:38 +0530

    DQL operation implemented

---
+++ 01-JDBC-CRUD/src/main/java/com/postgreSql/DQL/RetriveAllData.java
@@ -0,0 +1,39 @@
+package com.postgreSql.DQL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RetriveAllData {
+	public static void main(String[] args) {
+		
+		String url = "jdbc:postgresql://localhost:5432/mydb?user=postgres&password=root";
+		String query = "SELECT * FROM emp";
+		
+		try(Connection con = DriverManager.getConnection(url)){
+			Statement stm = con.createStatement();
+			ResultSet rs = stm.executeQuery(query);
+			
+			while(rs.next()) {
+				String id = rs.getString("empid");
+				String name = rs.getString("empname");
+				long salary = rs.getInt(3);
+				String designation = rs.getString(4);
+				
+				System.out.println(
+						"id = " + id + 
+						" name - " + name + 
+						" salary - " + salary + 
+						"  designation - " + designation);
+				
+			}
+			System.out.println();
+			System.out.println();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
